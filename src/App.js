15d03e16fbc328bcd9a87eefc1bededdc995318f
kaustin <krystalaustin99@gmail.com>
2023-04-27 23:24:21 -0500

    adding header styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers';
 import { CTA, Brand, Navbar } from './components';
+import './App.css';
 
 const App = () => {
   return (
@@ -13,6 +14,10 @@ const App = () => {
       <Brand />
       <WhatGPT3 />
       <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
